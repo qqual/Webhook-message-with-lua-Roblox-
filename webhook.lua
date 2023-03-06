f16f3731a7f59dd52c25a2cb0db5bb08127de02a
qqual <112658304+qqual@users.noreply.github.com>
2023-03-06 15:33:58 +0300

    Create webhook.lua

---
+++ webhook.lua
@@ -0,0 +1,27 @@
+
+--HOW TO SEND A WEBHOOK MESSAGE WITH LUA (ROBLOX) [USE IT WITH EXECUTOR]
+
+
+local url = "webhook"
+local data = {
+   ["content"] = "message(no embed, leave this blank if you gonna use embed also can be used with embed)", --you can take out embed by deleting the bottom stuff(where it says EMBEDS)
+   ["embeds"] = {
+       {
+           ["title"] = "title",
+           ["description"] = "desc",
+           ["type"] = "rich",
+           ["color"] = tonumber(0x7269da), --color
+           ["image"] = {
+               ["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username="..tostring(game:GetService("Players").LocalPlayer.Name)
+           }
+       }
+   }
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+   ["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef) --sending the message
